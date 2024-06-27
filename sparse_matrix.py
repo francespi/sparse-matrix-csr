4cb71ff53161ecf1f9c92b0d34218d90b2de3ceb
Francesco Pizzo <francesco.pizzo@outlook.com>
2024-06-27 21:31:20 +0200

    Add files via upload

---
+++ sparse_matrix.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+
+rows = 1024
+columns = 50304
+sparse_matrix = np.zeros((rows, columns))
+
+for i in range(rows):
+    for j in range(columns):
+        if i == j:
+            sparse_matrix[i][j] = 1
+
+sparse_matrix_csr = csr_matrix(sparse_matrix)
+
+def sparse_matrix_size(matrix):
+    return matrix.nbytes
+
+def csr_matrix_size(csr):
+    data_size = csr.data.nbytes
+    indices_size = csr.indices.nbytes
+    indptr_size = csr.indptr.nbytes
+    return data_size + indices_size + indptr_size
+
+sparse_size = sparse_matrix_size(sparse_matrix)
+csr_size = csr_matrix_size(sparse_matrix_csr)
+diff = sparse_size - csr_size
+diff_percentage = round(100*diff/sparse_size,8)
+
+print(f"Sparse matrix dimensions: {rows} x {columns}")
+print(f"Sparse matrix size: {sparse_size} bytes")
+print(f"CSR matrix size: {csr_size} bytes")
+print(f"Saving: {diff_percentage} %")
